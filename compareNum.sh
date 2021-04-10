ba585c8b2a3ebc4a1402716ead0e692399889996
sheetal <sheetalbedarkar96@gmail.com>
2021-04-10 19:04:48 +0530

    Added: check num is greater than or not

--- compareNum.sh
+++ compareNum.sh
@@ -1,11 +1,14 @@
 #!/bin/bash -x
 
-num1=10
+num1=20
 num2=10
 
 if [ $num1 -eq $num2 ]
 then
 	echo $num1 and $num2 are equal
+elif [ $num1 -gt $num2 ]
+then
+	echo $num1 is greater than $num2 
 else
-	echo $num1 and $num2 are not equal
+	echo $num1 is less than $num2
 fi
